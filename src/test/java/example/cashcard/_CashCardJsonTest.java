e3ece26105177e380b7019da8d249717cbefbd23
jaspreet-aneja <jaspreet.aneja13@gmail.com>
2024-05-24 14:09:48 +0530

    Initial commit and Basic JUnits

---
+++ src/test/java/example/cashcard/_CashCardJsonTest.java
@@ -0,0 +1,13 @@
+package example.cashcard;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class _CashCardJsonTest {
+	
+	@Test //@Test annotation is part of the JUnit library
+	void myFirstTest() {
+		//assertThat method is part of the AssertJ library
+		assertThat(42).isEqualTo(42);
+	}
+}
