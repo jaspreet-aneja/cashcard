202d35aef15bcaa1d43de0ddaedf1e75d458dff4
jaspreet-aneja <jaspreet.aneja13@gmail.com>
2024-06-29 22:37:37 +0530

    Added POST Endpoint

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,31 +1,42 @@
 package example.cashcard;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
 
 	private final CashCardRepository cashCardRepository;
-	
+
 	private CashCardController(CashCardRepository cashCardRepository) {
 		this.cashCardRepository = cashCardRepository;
 	}
-	
+
 	@GetMapping("/{requestedId}")
 	private ResponseEntity<CashCard> findById(@PathVariable("requestedId") Long requestedId) {
 		Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
-		
+
 		if (cashCardOptional.isPresent()) {
 			return ResponseEntity.ok(cashCardOptional.get());
 		} else {
 			return ResponseEntity.notFound().build();
 		}
+	}
+
+	@PostMapping
+	private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCard, UriComponentsBuilder ucb) {
+		CashCard createdCCard = cashCardRepository.save(newCashCard);
+		URI locationOfNewlyCreatedCCard = ucb.path("cashcards/{id}").buildAndExpand(createdCCard.id()).toUri();
+		return ResponseEntity.created(locationOfNewlyCreatedCCard).build();
 	}
 }
--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -12,6 +12,8 @@ import com.jayway.jsonpath.JsonPath;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.net.URI;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashcardApplicationTests {
 
@@ -39,5 +41,22 @@ class CashcardApplicationTests {
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 		assertThat(response.getBody()).isBlank();
 	}
-
+	
+	@Test
+	void shouldCreateANewCashCard() {
+		CashCard cashCard = new CashCard(null, 250.00);
+		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", cashCard, Void.class);
+		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+		
+		URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+		ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+		
+		DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+		Number id = documentContext.read("$.id");
+		Double amount = documentContext.read("$.amount");
+		
+		assertThat(id).isNotNull();
+		assertThat(amount).isEqualTo(250.00);
+	}
 }
