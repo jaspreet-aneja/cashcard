30704185b4ad06b1d5ef15159fb2097ba30ed99d
jaspreet-aneja <jaspreet.aneja13@gmail.com>
2024-06-30 22:29:06 +0530

    Implemented paging

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,8 +1,12 @@
 package example.cashcard;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,9 +43,12 @@ public class CashCardController {
 		URI locationOfNewlyCreatedCCard = ucb.path("cashcards/{id}").buildAndExpand(createdCCard.id()).toUri();
 		return ResponseEntity.created(locationOfNewlyCreatedCCard).build();
 	}
-	
-	@GetMapping()
-	private ResponseEntity<Iterable<CashCard>> findAll() {
-	   return ResponseEntity.ok(cashCardRepository.findAll());
+
+	@GetMapping
+	private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+		Page<CashCard> page = cashCardRepository
+				.findAll(PageRequest.of(pageable.getPageNumber(), pageable.getPageSize()));
+
+		return ResponseEntity.ok(page.getContent());
 	}
 }
